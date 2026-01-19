6fce0b1f654272305600d266c9c88a505edac8c2
GregoryRoadway <2407954@leedstrinity.ac.uk>
2026-01-19 15:36:57 +0000

    somet

--- Movement.gd
+++ Movement.gd
@@ -3,7 +3,10 @@ extends CharacterBody3D
 @onready var animated_sprite_3d: AnimatedSprite3D = $AnimatedSprite3D
 const speed = 5.0
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
