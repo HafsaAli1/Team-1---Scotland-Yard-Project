27f4dd7db3357cce3ad0f9eac2c181d3bc1f9ffd
ash464 <2405648@leedstrinity.ac.uk>
2026-02-25 13:07:03 +0000

    connected the map scene to the main menu so when the start button at the lobby is pressed the map page loads

--- final map design/menu/HTTP.gd
+++ final map design/menu/HTTP.gd
@@ -1,8 +1,8 @@
 extends Node
 
 var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
 @onready var http_request := HTTPRequest.new()
+
 func _ready():
 	add_child(http_request)
 	http_request.request_completed.connect(_on_request_completed)
@@ -10,27 +10,37 @@ func _ready():
 	var url = "http://trinity-developments.co.uk/games"
 	var error = http_request.request(url)
 	if error != OK:
-		print("Request Maps: ", error,"\n")
+		print("Request Maps Error: ", error)
+
 func _on_request_completed(result, response_code, headers, body):
+	# Safety Check: If we changed scenes, stop here
+	if not is_inside_tree(): 
+		return
+
 	if response_code == 200:
 		var json_string = body.get_string_from_utf8()
 		var json_data = JSON.parse_string(json_string)
+		
 		if typeof(json_data) == TYPE_DICTIONARY:
-			print("JSON Data: ", json_data)
+			var games = json_data.get("games", []) # Safer way to get data
+			var gameIdArray = []
+			
+			for i in games:
+				gameIdArray.append(i["gameId"])
+			
+			if gameIdArray.size() > 0:
+				var gameSelect = gameIdArray.pick_random()
+				
+				# Check if Label3 exists before assigning
+				if has_node("Label3"):
+					$Label3.text = str(gameSelect)
+				else:
+					print("Error: Label3 not found in scene tree.")
 		else:
 			print("Failed to parse JSON.")
-		var games = json_data["games"]
-		var gameIdArray = []
-		for i in games:
-			print("\n",i)
-			gameIdArray.append(i["gameId"])
-		print(gameIdArray)
-		if gameIdArray != []:
-			var gameSelect = gameIdArray.pick_random()
-			#var label := Label.new()  # Create a new Label node
-			$Label3.text = str(gameSelect)
-
 
 func _on_backbutton_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
-	
+
+func _on_startbutton_pressed():
+	get_tree().change_scene_to_file("res://map_scene.tscn")
