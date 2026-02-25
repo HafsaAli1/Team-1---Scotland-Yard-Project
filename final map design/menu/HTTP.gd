ce0bd7dcbd9e99e0d5acb9b7c30b8e9c5359e31f
GregoryRoadway <2407954@leedstrinity.ac.uk>
2026-02-25 14:55:12 +0000

    Save

--- final map design/menu/HTTP.gd
+++ final map design/menu/HTTP.gd
@@ -3,6 +3,8 @@ extends Node
 var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 @onready var http_request := HTTPRequest.new()
+@onready var button = $Button
+var check: bool = true
 func _ready():
 	add_child(http_request)
 	http_request.request_completed.connect(_on_request_completed)
@@ -10,7 +12,8 @@ func _ready():
 	var url = "http://trinity-developments.co.uk/games"
 	var error = http_request.request(url)
 	if error != OK:
-		print("Request Maps: ", error,"\n")
+		print("error: ", error,"\n")
+	button.pressed.connect(_on_button_pressed)
 func _on_request_completed(result, response_code, headers, body):
 	if response_code == 200:
 		var json_string = body.get_string_from_utf8()
@@ -19,13 +22,23 @@ func _on_request_completed(result, response_code, headers, body):
 			print("JSON Data: ", json_data)
 		else:
 			print("Failed to parse JSON.")
-		var games = json_data["games"]
-		var gameIdArray = []
-		for i in games:
-			print("\n",i)
-			gameIdArray.append(i["gameId"])
-		print(gameIdArray)
-		if gameIdArray != []:
-			var gameSelect = gameIdArray.pick_random()
-			#var label := Label.new()  # Create a new Label node
-			$Label3.text = str(gameSelect)
+		if check == true:
+			var gameIdArray = []
+			for i in json_data["games"]:
+				print("\n",i)
+				gameIdArray.append(i["gameId"])
+			print(gameIdArray)
+			if gameIdArray != []:
+				var gameSelect = gameIdArray.pick_random()
+				$Label3.text = str(int(gameSelect))
+				check = false
+		else:
+			print(json_data)
+	
+	
+func _on_button_pressed():
+	var headers = ["Content-Type: application/json"]
+	var url = "http://trinity-developments.co.uk/games" % [int($Label3.text)]
+	var error = http_request.request(url,headers,HTTPClient.METHOD_POST,json_data)
+	if error != OK:
+		print("Request Maps: ", error,"\n")
